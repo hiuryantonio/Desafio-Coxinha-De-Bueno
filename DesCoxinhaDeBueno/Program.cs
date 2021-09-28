8573b06361fce19bf582266e5b06df992ecaa9fb
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-28 15:20:50 -0300

    DesafioCoxinhaBuenoFinalizado

---
+++ DesCoxinhaDeBueno/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesCoxinhaDeBueno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(" ");
+
+            double coxinhas = int.Parse(line[0]);
+            double participantes = int.Parse(line[1]);
+            double media = coxinhas / participantes;
+
+            Console.WriteLine(media.ToString("N2"));
+        }
+    }
+}
